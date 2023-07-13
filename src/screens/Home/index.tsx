2dff4f1599007e81037ca33569e17b2362972548
Emerson Barbosa <emerson.silva@lucree.com.br>
2023-07-12 23:11:29 -0300

    refactor: typing routes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, Title } from './styles';
 import { CardLive } from '@components/CardLive'
 import { TouchableOpacity } from 'react-native';
 
 export function Home() {
+
+  const navigation = useNavigation();
+
+  function handleDetails(){
+    navigation.navigate('details');
+  }
+
   return (
     <Container>
       <Title>Partidas</Title>
-      <TouchableOpacity onPress={() => console.log('Card pressed')}>
+      <TouchableOpacity onPress={handleDetails}>
         <CardLive />
       </TouchableOpacity>
     </Container>
