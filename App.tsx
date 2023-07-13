1cecfa958a1163b3bfcaeb5b7b4e35ec779616f7
Emerson Barbosa <emerson.silva@lucree.com.br>
2023-07-12 23:40:18 -0300

    refactor: adding SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'react-native';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar, SafeAreaView } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_500Medium, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
@@ -12,11 +11,14 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-      />
-      {fontLoad ? <Routes /> : <ActivityIndicator />}
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar 
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontLoad ? <Routes /> : <ActivityIndicator />}
+      </SafeAreaView>
     </ThemeProvider>
   );
 }
