441494724a197306c0669b7f743620770c4873b6
Emerson Barbosa <emerson.silva@lucree.com.br>
2023-07-12 23:14:51 -0300

    feat: Initial new screen Details

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,14 +1,9 @@
 import { Container, Title } from './styles';
-import { CardLive } from '@components/CardLive'
-import { TouchableOpacity } from 'react-native';
 
 export function Details() {
   return (
     <Container>
-      <Title>Uhul</Title>
-      <TouchableOpacity onPress={() => console.log('Card pressed')}>
-        <CardLive />
-      </TouchableOpacity>
+      
     </Container>
   );
 }
